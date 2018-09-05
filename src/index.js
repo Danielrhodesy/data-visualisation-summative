112c6f25874477ee5e1fcf293840a7f10e71b54a
sebknight <37192621+sebknight@users.noreply.github.com>
2018-09-05 22:21:20 +1200

    installed reactstrap, imported webfonts

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import WebFont from 'webfontloader';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+
+WebFont.load({
+    google: {
+        families: ['Montserrat:400,700,900', 'sans-serif']
+    }
+})
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
