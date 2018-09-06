a125bdb97aee85b920786e27411bbedc4efe58f7
sebknight <37192621+sebknight@users.noreply.github.com>
2018-09-06 12:23:38 +1200

    added Gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function (grunt) {
+  grunt.initConfig({
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'expanded'
+        },
+        files: {                         // Dictionary of files
+          'src/index.css': 'src/index.scss'   // 'destination': 'source
+        }
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'src',
+          src: ['*.css', '!*.min.css'],
+          dest: 'src',
+          ext: '.min.css'
+        }]
+      }
+    },
+    watch: {
+      css: {
+        files: ["src/index.css", 
+      "src/App.css"],
+        tasks: ["cssmin"]
+      },
+      sass: {
+        files: ["src/index.scss", "src/App.scss"],
+        tasks: ["sass"]
+      }
+    }
+  });
+
+  // grunt.loadNpmTasks();
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.registerTask('w', ['watch']);
+};
