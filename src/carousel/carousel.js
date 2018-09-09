47077ed58ead3392a6d461f8fce83263027b834f
danielrhodesy <danielr@outlook.co.nz>
2018-09-10 08:57:46 +1200

    Updating repo from small changes

--- src/carousel/carousel.js
+++ src/carousel/carousel.js
@@ -8,7 +8,7 @@ import {
   Carousel
  } from 'reactstrap';
 // import { Carousel } from 'react-responsive-carousel';
-import Chart from "react-google-charts";
+// import Chart from "react-google-charts";
 
 class carousel extends Component {
   render() {
@@ -31,12 +31,12 @@ class carousel extends Component {
             </div>
           </div>
           <div className="graph1 graph-position">
-            <Chart
+            {/* <Chart
             chartType="ColumnChart"
             width="100%"
             height="400px"
             data={data}
-            />
+            /> */}
           </div>
           <div className="carousel">
           </div>
@@ -118,13 +118,13 @@ class carousel extends Component {
   }
 }
 
-const data = [
-  ["Element", "Density", { role: "style" }],
-  ["Copper", 8.94, "#b87333"], // RGB value
-  ["Silver", 10.49, "silver"], // English color name
-  ["Gold", 19.3, "gold"],
-  ["Platinum", 21.45, "color: #e5e4e2"] // CSS-style declaration
-];
+// const data = [
+//   ["Element", "Density", { role: "style" }],
+//   ["Copper", 8.94, "#b87333"], // RGB value
+//   ["Silver", 10.49, "silver"], // English color name
+//   ["Gold", 19.3, "gold"],
+//   ["Platinum", 21.45, "color: #e5e4e2"] // CSS-style declaration
+// ];
 // class App extends React.Component {
 //   render() {
 //     return (
