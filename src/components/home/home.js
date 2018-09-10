6446a1237ceab61d03bf66df2a690c94fffe0c52
Sebastian Maddox <sebastianknightmaddox@gmail.com>
2018-09-11 11:27:43 +1200

    more css changes incl structure

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -10,7 +10,7 @@ class Home extends Component {
 
       <div className="home">
         <div className="image-container">
-          <h1 className="app-title">Going<br></br> through it?<br/>You're not alone.</h1>
+          <h1 className="heading">Going<br></br> through it?<br/>You're not alone.</h1>
           <Button className="button" color="success" size="large">CHECK OUT THE STATS</Button>
           <Button className="button" color="success" size="large">FIND HELP NEAR YOU</Button>
         </div>
