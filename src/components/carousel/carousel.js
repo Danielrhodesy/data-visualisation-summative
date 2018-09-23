a322627c18c2c8529b7daefbf0595b17a03ed335
tashina romana jacobs <845865220@bun2-206-23.local>
2018-09-23 18:17:16 +1200

    data source added, some ui fix's

--- src/components/carousel/carousel.js
+++ src/components/carousel/carousel.js
@@ -145,7 +145,7 @@ class Carousel extends Component {
             <div className="slider-background">
             </div>
           <div className="button-container">
-            <Button color="success" size="large" onClick={this.props.changePage.bind(this, 'MapPage')}>FIND HELP</Button>
+            <Button color="success" size="large" onClick={this.props.changePage.bind(this, 'MapPage')}>FIND HELP NEAR YOU</Button>
           </div>
         </div>
       </div>
@@ -179,6 +179,7 @@ class Slide1 extends Component {
           <p className="paragraph paragraph1">
             171,033 people accessed mental health care and addiction services in 2015 - 16.
           </p>
+          <p className="credit">-Ministry of Health - 'Mental Health and Addiction: Service Use 2015/16'</p>
         </div>
       </div>
     )
@@ -193,6 +194,7 @@ class Slide2 extends Component {
           <p className="paragraph paragraph2">
             Kiwis are reporting higher reates of psychological distress each year, meaning our <b>mental health care services are more important than ever.</b>
           </p>
+          <p className="credit">-Ministry of Health - Ministry of Health - 'New Zealand Health Survey'</p>
         </div>
       </div>
     );
@@ -211,8 +213,9 @@ class Slide3 extends Component {
             Who's Getting Help?
           </p>
           <p className="paragraph paragraph3">
-            Per capita, Maori are seeking help the most often -- 6450 people per 100,000, compared to 1125 per 100,000 Asian people.
+            Per capita, Maori are seeking the most help  -- 6450 people per 100,000, compared to 1125 per 100,000 Asian people.
           </p>
+          <p className="credit">-Ministry of Health - 'Mental Health and Addiction: Service Use 2015/16'</p>
         </div>
       </div>
 
@@ -231,8 +234,9 @@ class Slide4 extends Component {
             Who's Getting Help?
           </p>
           <p className="paragraph paragraph3">
-            Per capita, Maori are seeking help the most often -- 6450 people per 100,000, compared to 1125 per 100,000 Asian people.
+            Men and women are accessing the mental health services at about the same rate.
           </p>
+          <p className="credit">-Ministry of Health - 'Mental Health and Addiction: Service Use 2015/16'</p>
         </div>
       </div>
     );
@@ -257,7 +261,7 @@ var settings = {
 // BAR CHART DATA
 
 const bardata = [
-  ["Age", "People", { role: "style" }],
+  ["Age", "Number Of People By Age", { role: "style" }],
   ["Under 15", 25661, "#5A496A"],
   ["15 - 24", 40436, " #5A496A"],
   ["25 - 34", 30505, " #5A496A"],
@@ -268,27 +272,32 @@ const bardata = [
 ];
 
 const baroptions = {
-  legend: {position: 'none'},
-  chartArea:{left:65,top:50,width:'75%',height:'65%'}
+  title:'PEOPLE ACCESSING SERVICES',
+  titleTextStyle: {color: 'grey', fontName: 'Montserrat'},
+  chartArea:{left:65,top:50,width:'75%',height:'65%'},
+  legend: {position: 'bottom'},
+  colors: ["#5A496A"]
 }
 
 // LINE CHART DATA
 
 const linedata = [
-  ["Year", "Reports"],
-  ["2011", 56.4],
-  ["2012", 58.9],
-  ["2013", 65.5],
-  ["2014", 61.6],
-  ["2015", 66.9],
-  ["2016", 72.4]
+  ["Year", "Percentage By Year (15yrs and older)"],
+  ["2011", 12.7],
+  ["2012", 16.3],
+  ["2013", 16.4],
+  ["2014", 17.4],
+  ["2015", 18.8],
+  ["2016", 19.9]
 ];
 
 const lineoptions = {
   curveType: "function",
+  title:'REPORTS OF MOOD AND ANXIETY DISORDER',
+  titleTextStyle: {color: 'grey', fontName: 'Montserrat'},
   colors: ["#EF5D60"],
-  legend: {position: 'none'},
-  chartArea:{left:65,top:50,width:'70%',height:'65%'}
+  chartArea:{left:65,top:50,width:'70%',height:'65%'},
+  legend: {position: 'bottom',}
 };
 
 // PIE CHART DATA
@@ -304,8 +313,11 @@ const donutdata1 = [
 
 const donutoptions1 = {
   pieHole: 0.32,
-  chartArea:{left:60,top:50,width:'70%',height:'50%'},
-  legend: {position: 'labeled'}
+  title:'SERVICE USE BY ETHNICITY',
+  titleTextStyle: {color: 'grey', fontName: 'Montserrat'},
+  slices: {0: {color: '#866BB6'}, 1: {color: '#EF5D60'}, 2: {color: '#5A496A'}, 3: {color: '#95BB4D'}},
+  chartArea:{left:60,top:80,width:'75%',height:'55%'},
+  legend: {position: 'bottom'}
 }
 
 // PIE CHART 2
@@ -318,8 +330,12 @@ const donutdata2 = [
 
 const donutoptions2 = {
   pieHole: 0.32,
-  chartArea:{left:60,top:50,width:'70%',height:'50%'},
-  legend: {position: 'right'}
+  // title: position: 'bottom',
+  title: 'SERVICE USE BY GENDER', fontSize: '80px',
+  titleTextStyle: {color: 'grey', fontName: 'Montserrat',},
+  slices: {0: {color: '#5A496A'}, 1: {color: '#EF5D60'}},
+  chartArea:{left:60,top:80,width:'75%',height:'55%'},
+  legend: {position: 'bottom'}
 };
 
 
